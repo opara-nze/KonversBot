605150c90acb97ca7f97dd37c5bf1cde00b39bfb
Opara Nze <opara.nze@gmx.com>
2019-07-20 20:52:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//sidenav
+const sideNav = document.querySelector(".sidenav");
+M.Sidenav.init(sideNav, {});
+
+//ScrollSpy
+const ss = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(ss, {});
+
+//floating button
+const actionBtn = document.querySelector(".fixed-action-btn");
+M.FloatingActionButton.init(actionBtn, {});
